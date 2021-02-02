e02842964c603bd8d00d2420da0d340f6fbb8ff9
pitor97 <uzzolbesra@gmail.com>
2021-02-02 10:43:31 +0100

    this the summary of null vs undefinde

---
+++ nullVsUndefined.js
@@ -0,0 +1,51 @@
+//let pakhi;
+//console.log(pakhi); // the result is undefine because we did not set the value
+
+//function add(num1, num2) {
+//  console.log(num1 + num2);
+//   the result is undefined because here we did not return any value
+//}
+//const result = add(13, 85);
+//console.log(result);
+
+//function add(num1, num2) {
+//  console.log(num1 + num2);
+//  return  // the result is undefined because here we just write the return but then after we did not set the value
+//}
+//const result = add(13, 85);
+//console.log(result);
+
+//function add(num1, num2) {
+//    console.log(num1, num2 );
+//
+//}
+//  const result = add(13, ); ///here is also undefine value because we did not pass the parameter
+//  console.log(result);
+
+//function add(num1, num2 = 10) {
+//  ///// this is the solution we put the value of num2 = in the parameter
+//  console.log(num1, num2);
+//}
+//const result = add(13);
+//console.log(result);
+//
+
+/// Undefined as object
+
+//const lover = { name: "smart dude", Phone: 017388851 };
+//console.log(lover.girlfriend); /// * here we get the undefined value because we did not set the girlfriend element in this object
+//
+
+//let fun = undefined;
+//console.log(fun); // we got the undefined vale because we set it as a undefined but its not recomended
+
+null = empty there is nothing 
+
+ // we use the null to check out privious value if there is any 
+ // null = meaning there is nothing not exist 
+
+//# we can get the undefined value in several way such as if dot pass the parameter , undefined mean false . 
+// if there is not a exist property but we want to acces that so that we can get the undefined 
+
+let ages = [2, 5,9]
+console.log(ages[11]); // here we get the undefined because in that array postion number 11 is absenent 
