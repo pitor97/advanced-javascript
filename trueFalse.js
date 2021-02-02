5845c5e5254a5cb65a1dc858dba478bb06e5ad22
pitor97 <uzzolbesra@gmail.com>
2021-02-02 09:47:59 +0100

    trur vs false

---
+++ trueFalse.js
@@ -0,0 +1,111 @@
+//const age = 4;
+//
+//if (age > 0) {
+//  console.log("condition is true");
+//} else {
+//  console.log("condition is false");
+//}
+//
+
+//const name = "UzzolSuli";
+//
+//if (name.length > 0) {
+//  console.log("condition is true");
+//} else {
+//  console.log("condition is false");
+//}
+// we can check the variable another way also
+
+// const name = "UzzolSuli";
+//
+//if (name) {               // here is the deference
+//    console.log("condition is True");
+//} else {
+//    console.log("condition is false");
+//}
+
+/// const name = "UzzolSuli";   if we give the empty string our result will be zero
+//const name = ""; /// either we if we give the space between the string then it will count the as a element
+//
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+
+//// * we can say that every single number is truth
+/// accept zero 0
+/// ** every string is true other then empty string
+
+//let name; //// * for the undefined element java script will count as false
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+
+//let name = null;   // if we set our value as null then java script is cosider it as a false
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+
+//let name = false;  // if we set out value like a false it will be false
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+
+//** */ as far we know that
+// 0 = false
+// "" = false
+// undefined = false
+// null = false
+// NaN = false
+// NaN = not a Number
+
+//let name = NaN;
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+////***  */ on the other hand  true condition
+
+// "0" = between the colone 0 will  bec count as a string so "0 " is true
+// " " = empty string i mean gap between the colone is consider there is some element so it will be true
+// [] = empty array is true because its take place
+// { } = this is also give the value true
+// "false" = true 
+
+
+//let name = "false"; //  if we write false between the qutation mark that will be true because it take place and java cosnider it length
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
+
+// let name = [];  // []= array is consider as true
+//  console.log(name);
+// if (name) {
+//   console.log("condition is True");
+// } else {
+//   console.log("condition is false");
+// }
+//
+
+// let name = {};  // object is also true
+//console.log(name);
+//if (name) {
+//  console.log("condition is True");
+//} else {
+//  console.log("condition is false");
+//}
